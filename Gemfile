3c1baca53e57b3bc2acd33bc83fbadefc5677f6c
Jair Silva <jroliveira.sa@gmail.com>
2021-10-07 12:28:21 -0300

    Criando Paginaçao

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jbuilder', '~> 2.7'
 
 # Authentication 
 gem 'devise'
+# Pagination
+gem 'kaminari'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'blog/index'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrtions: 'users/registrations' 
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'users/sign_out' => 'devise/sessions#destroy'
   end
+  
+  resources :blog, only: [:index]
   
  root 'home#index'
 end
